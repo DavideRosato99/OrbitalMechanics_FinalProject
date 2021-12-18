bd43bfced6915aaab65942c6135f49afd2ac5a61
Davide Rosato <73779437+DavideRosato99@users.noreply.github.com>
2021-12-18 12:45:18 +0100

    Update teme2eci.m

--- Assignment1/functions/initialize/teme2eci.m
+++ Assignment1/functions/initialize/teme2eci.m
@@ -1,13 +1,42 @@
-function [rECI, vECI] = teme2eci(rTEME, vTEME, timemjd2000)
+function [rECI, vECI, tm] = teme2eci(rTEME, vTEME, timemjd2000)
+% teme2eci - function to compute the rotation matrix to perform a
+% transformation from TEME (True Equator Mean Equinox) reference frame to
+% ECI Geocentric Equatorial Reference Frame (IAU-76/FK5 J2000, mean
+% equator, mean equinox frame).
+%
+% PROTOTYPE
+%   [rECI,vECI]=teme2eci(rTEME, vTEME, timemjd2000)
+%
+% INPUT:
+%   rTEME        double  [Nx3]   positions vectors (TEME)            [km]
+%   vTEME        double  [Nx3]   velocities vectors (TEME)         [km/s]
+%   timemjd2000  double  [1x1]   modified Julian date (J2000)         [d]
+%
+% OUTPUT:
+%   rECI         double  [Nx3]   positions vectors (ECI)             [km]
+%   vECI         double  [Nx3]   velocities vectors (ECI)          [km/s]
+%   tm           double  [3x3]   rotation matrix (TEME -> ECI)        [-]
+%
+% CALLED FUNCTIONS: precess, nutation
+%
+% REFERENCES:
+%   David Vallado       2013, 231-233
+%
+% CONTRIBUTORS:
+%   Rosato Davide               10618468
+%   Saba Mohammadi Yengeje      10789462
+%   Spinelli jason              10618465
+%   Tagliati Alessia            10635119
+%
+% VERSIONS
+%   2021-10-21: Release
+%
+% -------------------------------------------------------------------------
 
-if size(rTEME, 1) == 1
-    rTEME = rTEME';
-end
+% Get size of the positions (and velocities) vector
+N = size(rTEME, 1);
 
-if size(vTEME, 1) == 1
-    vTEME = vTEME';
-end
-
+%% CALCULATE ROTATION MATRIX
 ddpsi = -0.054522 * pi / (180*3600);
 ddeps = -0.006209 * pi / (180*3600);
 centjd = (timemjd2000)/(36525);
@@ -32,5 +61,10 @@ eqe(3,3) =  1.0;
 
 tm = prec * nut * eqe';
 
-rECI = tm * rTEME;
-vECI = tm * vTEME;
+%% ROTATE VECTORS
+rECI = zeros(N, 3);
+vECI = zeros(N, 3);
+for i = 1:N
+    rECI(i, 1:3) = tm * rTEME(i, 1:3)';
+    vECI(i, 1:3) = tm * vTEME(i, 1:3)';
+end
