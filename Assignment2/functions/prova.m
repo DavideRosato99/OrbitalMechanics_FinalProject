d425b726b091944901784f7a612a91d0d6d95bc0
Davide Rosato <73779437+DavideRosato99@users.noreply.github.com>
2021-12-28 18:13:41 +0100

    xdcd

--- Assignment2/functions/prova.m
+++ Assignment2/functions/prova.m
@@ -35,7 +35,7 @@ periodArrPlanet   = 2*pi * sqrt(kepArr(1)^3 / ksun);
 
 % Synodic period
 synPeriod1 = (periodFlyByPlanet * periodDepPlanet)/(abs(periodFlyByPlanet - periodDepPlanet))
-synPeriod2 = (periodArrPlanet * periodFlyByPlanet)/(abs(periodArrPlanet - periodFlyByPlanet));
+synPeriod2 = (periodArrPlanet * periodFlyByPlanet)/(abs(periodArrPlanet - periodFlyByPlanet))
 
 %% TOF VECTORS
 arrFlyByVec = linspace(0, TOTdays*24*3600, nTOF1);
@@ -44,9 +44,11 @@ arrArrVec   = linspace(0, TOTdays*24*3600, nTOF2);
 %% CALCULATION
 DVfirstLeg = nan(nDep, nTOF1);
 DVsecondLeg = nan(nDep, nTOF1, nTOF2);
+DVsecondLeg2 = nan(nTOF1, nTOF2);
 
 nDep
-for i = 1:nDep
+flag = false;
+parfor i = 1:nDep
     disp(i)
     depDateLoop = depDate;
     depDateLoop(6) = depDateLoop(6) + secFromDep(i);
@@ -81,14 +83,9 @@ for i = 1:nDep
                 [rrFlyBy, vvFlyBy] = par2car(kepFlyBy, ksun);
 
                 TOF1 = (datenum(FlyByDateLoop) - datenum(depDateLoop))*24*3600;
-                [~, ~, ~, err, vI, vF, ~, ~] = lambertMR(rrDep, rrFlyBy, TOF1, ksun, 0, 0, 0, 1);
-
+                [~, ~, ~, err, vI_1, vF_1, ~, ~] = lambertMR(rrDep, rrFlyBy, TOF1, ksun, 0, 0, 0, 1);
 
                 if err == 0
-
-                    DV1 = norm(vI' - vvDep);
-                    DV2 = norm(vF' - vvFlyBy);
-                    DVfirstLeg(i, j) = DV1 + DV2;
 
                     for k = 1:nTOF2
 
@@ -99,25 +96,35 @@ for i = 1:nDep
                         [HLL, MLL, SLL]  = hms(datetime(arrDateLoop));
 
                         datemjd2000Ll = date2mjd2000([YLL MoLL DLL HLL MLL SLL]);
+                        
+                        controlConsL = datenum(arrDateLoop) - datenum(FlyByDateLoop);
+                        
+                        if controlConsL >= 0
+                        
+                            controlDateL = datenum(arrDate) - datenum(arrDateLoop);
 
-                        controlDateL = datenum(arrDate) - datenum(arrDateLoop);
+                            if controlDateL >= 0
 
-                        if controlDateL >= 0
+                                [kepArr, ksun] = uplanet(datemjd2000Ll, arrPlanID);
+                                [rrArr, vvArr] = par2car(kepArr, ksun);
 
-                            [kepArr, ksun] = uplanet(datemjd2000Ll, arrPlanID);
-                            [rrArr, vvArr] = par2car(kepArr, ksun);
+                                TOF2 = (datenum(arrDateLoop) - datenum(FlyByDateLoop))*24*3600;
+                                [~, ~, ~, err, vI_2, vF_2, ~, ~] = lambertMR(rrFlyBy, rrArr, TOF2, ksun, 0, 0, 0, 1);
 
-                            TOF2 = (datenum(arrDateLoop) - datenum(depDate))*24*3600;
-                            [~, ~, ~, err, vI, vF, ~, ~] = lambertMR(rrFlyBy, rrArr, TOF2, ksun, 0, 0, 0, 1);
+                                if err == 0
+                                    
+                                    DV1_1 = norm(vI_1' - vvDep);
+                                    DV2_1 = norm(vF_1' - vvFlyBy);
+                                    DVfirstLeg(i, j) = DV1_1;
 
-                            if err == 0
+                                    DV1_2 = norm(vI_2' - vvFlyBy);
+                                    DV2_2 = norm(vF_2' - vvArr);
+                                    DVsecondLeg(i, j, k) = DV2_2;
 
-                                DV1 = norm(vI' - vvFlyBy);
-                                DV2 = norm(vF' - vvArr);
-                                DVsecondLeg(i, j, k) = DV1 + DV2;
+                                end
 
                             end
-
+                        
                         end
 
 
@@ -134,12 +141,187 @@ for i = 1:nDep
     
 end
 
+%%
+for j = 1:nTOF1
+    for k = 1:nTOF2
+        DV = DVsecondLeg(:, j, k);
+        
+        minDV = min(DV);
+        
+        DVsecondLeg2(j, k) = minDV;
+        
+    end
+end
+
+%%
+minDV = zeros(floor(TOTdays*24*3600/synPeriod2)+1, 1);
+minDVdays = zeros(floor(TOTdays*24*3600/synPeriod2)+1, 1);
+minima = min(DVsecondLeg2, [], 2);
+
+for i = 1:floor(TOTdays*24*3600/synPeriod2)+1
+    indexes = find(arrFlyByVec >= (i-1)*synPeriod2 + 1 & arrFlyByVec < i*synPeriod2);
+    [minDV(i), ind] = min(minima(indexes));
+    ciao = arrFlyByVec(indexes);
+    minDVdays(i) = ciao(ind)/(24*3600);
+end
+
+maxMinDV = max(minDV);
+minMinDV = min(minDV);
+
+f = @(DV) (maxMinDV - DV).^4/(maxMinDV - minMinDV)^4;
+
+figure
+plot(minDVdays, minDV); grid on
+
+
+dateOptFlyBy = (sum(minDVdays(not(isnan(minDV))).*f(minDV(not(isnan(minDV))))))/(sum(f(minDV(not(isnan(minDV))))));
+
+
+figure
+plot(minDVdays, f(minDV)); hold on; grid on
+xline(dateOptFlyBy)
+
+%%%
+DVfirstLeg2 = nan(nDep, 1);
+secFromDep2 = linspace(0, dateOptFlyBy*24*3600, nDep);
+for i = 1:nDep
+    depDateLoop2 = depDate;
+    depDateLoop2(6) = depDateLoop2(6) + secFromDep2(i);
+    
+    [Yl, Mol, Dl] = ymd(datetime(depDateLoop2));
+    [Hl, Ml, Sl]  = hms(datetime(depDateLoop2));
+    
+    datemjd2000l = date2mjd2000([Yl Mol Dl Hl Ml Sl]);
+    
+    % Departure planet
+    [kepDep, ksun] = uplanet(datemjd2000l, depPlanID);
+    [rrDep, vvDep] = par2car(kepDep, ksun);
+    
+    % FlyBy planet
+    FlyByDateLoop2 = depDate;
+    FlyByDateLoop2(6) = FlyByDateLoop2(6) + dateOptFlyBy*24*3600;
+    [YL, MoL, DL] = ymd(datetime(FlyByDateLoop2));
+    [HL, ML, SL]  = hms(datetime(FlyByDateLoop2));
+    datemjd2000L = date2mjd2000([YL MoL DL HL ML SL]);
+                
+    [kepFlyBy, ksun] = uplanet(datemjd2000L, flyByPlanID);
+    [rrFlyBy, vvFlyBy] = par2car(kepFlyBy, ksun);
+    
+    TOF1 = (dateOptFlyBy*24*3600) - secFromDep2(i);
+    [~, ~, ~, err, vI_1, vF_1, ~, ~] = lambertMR(rrDep, rrFlyBy, TOF1, ksun, 0, 0, 0, 1);
+    if err == 0
+        DV1_1 = norm(vI_1' - vvDep);
+        DV2_1 = norm(vF_1' - vvFlyBy);
+        DVfirstLeg2(i) = DV1_1;
+    end
+    
+end
+
+for i = 1:floor(dateOptFlyBy*24*3600/synPeriod1)+1
+    indexes = find(secFromDep2 >= (i-1)*synPeriod1 + 1 & secFromDep2 < i*synPeriod1);
+    [minDV2(i), ind] = min(DVfirstLeg2(indexes));
+    ciao = secFromDep2(indexes);
+    minDVdays2(i) = ciao(ind)/(24*3600);
+end
+
+maxMinDV = max(minDV2);
+minMinDV = min(minDV2);
+
+f = @(DV) (maxMinDV - DV).^4/(maxMinDV - minMinDV)^4;
+
+figure
+plot(minDVdays2, minDV2); grid on
+
+
+dateOptDep = (sum(minDVdays2(not(isnan(minDV2))).*f(minDV2(not(isnan(minDV2))))))/(sum(f(minDV2(not(isnan(minDV2))))));
+
+
+figure
+plot(minDVdays2, f(minDV2)); hold on; grid on
+xline(dateOptDep)
+
+
+dateOptDep = dateOptDep;
+date = depDate;
+date(6) = date(6) + dateOptDep*24*3600;
+[YL, MoL, DL] = ymd(datetime(date));
+[HL, ML, SL]  = hms(datetime(date));
+dateOptDepString = datestr([YL MoL DL HL ML SL])
+
+%%%
+arrArrVec2 = linspace(dateOptFlyBy*24*3600, TOTdays*24*3600, nTOF2);
+DVsecondLeg3 = nan(nDep, 1);
+for i = 1:nTOF2
+    depDateLoop3 = depDate;
+    depDateLoop3(6) = depDateLoop3(6) + arrArrVec2(1);
+    
+    [Yl, Mol, Dl] = ymd(datetime(depDateLoop3));
+    [Hl, Ml, Sl]  = hms(datetime(depDateLoop3));
+    
+    datemjd2000l = date2mjd2000([Yl Mol Dl Hl Ml Sl]);
+    
+    % FlyBy planet
+    [kepFlyBy, ksun] = uplanet(datemjd2000l, flyByPlanID);
+    [rrFlyBy, vvFlyBy] = par2car(kepFlyBy, ksun);
+    
+    % Arr planet
+    ArrDateLoop3 = depDate;
+    ArrDateLoop3(6) = ArrDateLoop3(6) + arrArrVec2(i);
+    [YL, MoL, DL] = ymd(datetime(ArrDateLoop3));
+    [HL, ML, SL]  = hms(datetime(ArrDateLoop3));
+    datemjd2000L = date2mjd2000([YL MoL DL HL ML SL]);
+                
+    [kepArr, ksun] = uplanet(datemjd2000L, arrPlanID);
+    [rrArr, vvArr] = par2car(kepArr, ksun);
+    
+    TOF2 = arrArrVec2(i) - dateOptFlyBy*24*3600;
+    [~, ~, ~, err, vI_2, vF_2, ~, ~] = lambertMR(rrFlyBy, rrArr, TOF2, ksun, 0, 0, 0, 1);
+    if err == 0
+        DV1_2 = norm(vI_2' - vvFlyBy);
+        DV2_2 = norm(vF_2' - vvArr);
+        DVsecondLeg3(i) = DV2_2;
+    end
+    
+end
+
+for i = 1:floor((TOTdays - dateOptFlyBy)*24*3600/synPeriod2)+1
+    indexes = find(arrArrVec2 >= (i-1)*synPeriod2 + dateOptFlyBy*24*3600 + 1 & arrArrVec2 < i*synPeriod2 + dateOptFlyBy*24*3600);
+    [minDV3(i), ind] = min(DVsecondLeg3(indexes));
+    ciao = arrArrVec2(indexes);
+    minDVdays3(i) = ciao(ind)/(24*3600);
+end
+
+maxMinDV = max(minDV3);
+minMinDV = min(minDV3);
+
+f = @(DV) (maxMinDV - DV).^2/(maxMinDV - minMinDV)^2;
+
+figure
+plot(minDVdays3, minDV3); grid on
+
+
+dateOptArr = (sum(minDVdays3(not(isnan(minDV3))).*f(minDV3(not(isnan(minDV3))))))/(sum(f(minDV3(not(isnan(minDV3))))));
+
+
+figure
+plot(minDVdays3, f(minDV3)); hold on; grid on
+xline(dateOptArr)
+
+
+dateOptArr = dateOptArr;
+date = depDate;
+date(6) = date(6) + dateOptArr*24*3600;
+[YL, MoL, DL] = ymd(datetime(date));
+[HL, ML, SL]  = hms(datetime(date));
+dateOptArrString = datestr([YL MoL DL HL ML SL])
+
 
 %%
 data.timeWindows.depDays = secFromDep/(24*3600);
 data.timeWindows.DVfirstLeg = DVfirstLeg;
 data.timeWindows.FlyByDays = arrFlyByVec/(24*3600);
 data.timeWindows.DVsecondLeg = DVsecondLeg;
+data.timeWindows.DVsecondLeg2 = DVsecondLeg2;
 data.timeWindows.arrDays = arrArrVec/(24*3600);
 
 %%
@@ -168,32 +350,59 @@ end
 
 %%
 daysSyn1 = synPeriod1/(24*3600);
+daysSyn2 = synPeriod2/(24*3600);
 
 if settings.timeWindows.plot
     
+    figure
     [A, B] = meshgrid(data.timeWindows.depDays, data.timeWindows.FlyByDays);
     contour(A, B, data.timeWindows.DVfirstLeg', 100)
     xticks(vec2); xticklabels(vec1); xtickangle(-45)
     yticks(vec4); yticklabels(vec3)
-    hold on
-    plot([0 daysSyn1], [daysSyn1 0], 'r');
-    plot([0 2*daysSyn1], [2*daysSyn1 0], 'r');
-    plot([0 3*daysSyn1], [3*daysSyn1 0], 'r');
-    plot([0 4*daysSyn1], [4*daysSyn1 0], 'r');
-    plot([0 5*daysSyn1], [5*daysSyn1 0], 'r');
-    plot([0 6*daysSyn1], [6*daysSyn1 0], 'r');
-    plot([0 7*daysSyn1], [7*daysSyn1 0], 'r');
-    plot([0 8*daysSyn1], [8*daysSyn1 0], 'r');
-    plot([0 9*daysSyn1], [9*daysSyn1 0], 'r');
-    plot([0 10*daysSyn1], [10*daysSyn1 0], 'r');
+    xlabel('Mars Departure Date'); ylabel('Earth Arrival Date')
+    title('$\Delta$V First Leg Porkchop')
     
     figure
-    plot(data.timeWindows.FlyByDays, data.timeWindows.DVfirstLeg(1, :))
-    hold on
-    plot([2*daysSyn1 2*daysSyn1], [0 max(data.timeWindows.DVfirstLeg(1, :))], 'r');
-    plot([3*daysSyn1 3*daysSyn1], [0 max(data.timeWindows.DVfirstLeg(1, :))], 'r');
-    plot([4*daysSyn1 4*daysSyn1], [0 max(data.timeWindows.DVfirstLeg(1, :))], 'r');
-    plot([5*daysSyn1 5*daysSyn1], [0 max(data.timeWindows.DVfirstLeg(1, :))], 'r');
+    [A, B] = meshgrid(data.timeWindows.FlyByDays, data.timeWindows.arrDays);
+    contour(A, B, data.timeWindows.DVsecondLeg2', 100)
+    xticks(vec2); xticklabels(vec1); xtickangle(-45)
+    yticks(vec4); yticklabels(vec3)
+    xlabel('Earth Departure Date'); ylabel('Venus Arrival Date')
+    title('$\Delta$V Second Leg Porkchop')
+    
+    figure
+    minima1 = min(data.timeWindows.DVfirstLeg(1:end-5,:), [], 2);
+    plot(data.timeWindows.depDays(1:end-5), minima1, '-o', 'MarkerSize', 5, 'LineWidth', 1)
+    hold on; grid on;
+    for i = 1:floor(TOTdays/daysSyn1)
+        xline(i*daysSyn1, '--k');
+    end
+    xticks(vec2); xticklabels(vec1); xtickangle(-45)
+    xlabel('Mars Departure Date'); ylabel('min $\Delta$V [$\frac{km}{s}$]')
+    title('min $\Delta$V First Leg')
+    
+    figure
+    minima2 = min(data.timeWindows.DVsecondLeg2(1:end-5,:), [], 2);
+    plot(data.timeWindows.FlyByDays(1:end-5), minima2, '-o', 'MarkerSize', 5, 'LineWidth', 1)
+    hold on; grid on;
+    for i = 1:floor(TOTdays/daysSyn2)
+        xline(i*daysSyn2, '--k');
+    end
+    xticks(vec4); xticklabels(vec3); xtickangle(-45)
+    xlabel('Earth Departure Date'); ylabel('min $\Delta$V [$\frac{km}{s}$]')
+    title('min $\Delta$V Second Leg')
+    
+    figure
+    minima3 = min(data.timeWindows.DVsecondLeg(1:end-5,:), [], [2 3]);
+    plot(data.timeWindows.depDays(1:end-5), minima3, '-o', 'MarkerSize', 5, 'LineWidth', 1)
+    hold on; grid on;
+    for i = 1:floor(TOTdays/daysSyn2)
+        xline(i*daysSyn2, '--k');
+    end
+    xticks(vec2); xticklabels(vec1); xtickangle(-45)
+    xlabel('Mars Departure Date'); ylabel('min $\Delta$V [$\frac{km}{s}$]')
+    title('min $\Delta$V Second Leg')
+   
     
     
     
